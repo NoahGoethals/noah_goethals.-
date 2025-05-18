945ec3dbfd6a7e0f404f3b1da80391d1936f0224
NoahGoethals <noah.goethals@student.ehb.be>
2025-05-18 10:51:55 +0200

    6de commit: details pagina en route toegevoegd

--- src/main/java/be/ehb/noah_goethals/EventController.java
+++ src/main/java/be/ehb/noah_goethals/EventController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 import java.util.List;
 
@@ -24,4 +26,11 @@ public class EventController {
         model.addAttribute("events", events);
         return "index";
     }
+    @GetMapping("/details/{id}")
+    public String showDetails(@PathVariable("id") Long id, Model model) {
+        Event event = eventRepository.findById(id).orElse(null);
+        model.addAttribute("event", event);
+        return "details";
+    }
+
 }
